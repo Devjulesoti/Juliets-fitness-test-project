9fd5fd5adbed0f75515f0c51d502f9f138611cf0
juliet oti <egwuotijulietchinenye@gmail.com>
2022-07-28 01:24:53 +0100

    init

---
+++ index.js
@@ -0,0 +1,65 @@
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
+
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
+
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
+
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
+
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
+
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
+
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
+
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
+
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
+
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
+
+const colorchange = document.getElementById("caption");
+colorchange.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "orange";
+});
