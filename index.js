b7b5bdd7b5c7197f90745bedb8a3b99de1348e4d
juliet oti <egwuotijulietchinenye@gmail.com>
2022-07-28 01:44:53 +0100

    commit2

--- index.js
+++ index.js
@@ -4,62 +4,74 @@ colorchange.addEventListener("click", (event) =>{
     event.target.style.color = "orange";
 });
 
-const colorchange = document.getElementById("caption");
-colorchange.addEventListener("click", (event) =>{
-    event.target.style.background = "green";
-    event.target.style.color = "orange";
+const colorchange1 = document.getElementById("caption1");
+colorchange1.addEventListener("click", (event) =>{
+    event.target.style.background = "purple";
+    event.target.style.color = "yellow";
 });
 
-const colorchange = document.getElementById("caption");
-colorchange.addEventListener("click", (event) =>{
-    event.target.style.background = "green";
-    event.target.style.color = "orange";
+const colorchange2 = document.getElementById("caption2");
+colorchange2.addEventListener("click", (event) =>{
+    event.target.style.background = "black";
+    event.target.style.color = "white";
 });
 
-const colorchange = document.getElementById("caption");
-colorchange.addEventListener("click", (event) =>{
+const colorchange3 = document.getElementById("caption3");
+colorchange3.addEventListener("click", (event) =>{
     event.target.style.background = "green";
-    event.target.style.color = "orange";
+    event.target.style.color = "white";
 });
 
-const colorchange = document.getElementById("caption");
-colorchange.addEventListener("click", (event) =>{
-    event.target.style.background = "green";
-    event.target.style.color = "orange";
+const colorchange4 = document.getElementById("caption4");
+colorchange4.addEventListener("click", (event) =>{
+    event.target.style.background = "pink";
+    event.target.style.color = "white";
 });
 
-const colorchange = document.getElementById("caption");
-colorchange.addEventListener("click", (event) =>{
+const colorchange5 = document.getElementById("caption5");
+colorchange5.addEventListener("click", (event) =>{
     event.target.style.background = "green";
-    event.target.style.color = "orange";
+    event.target.style.color = "white";
 });
 
-const colorchange = document.getElementById("caption");
-colorchange.addEventListener("click", (event) =>{
-    event.target.style.background = "green";
-    event.target.style.color = "orange";
+const colorchange6 = document.getElementById("caption6");
+colorchange6.addEventListener("click", (event) =>{
+    event.target.style.background = "blue";
+    event.target.style.color = "white";
 });
 
-const colorchange = document.getElementById("caption");
-colorchange.addEventListener("click", (event) =>{
-    event.target.style.background = "green";
-    event.target.style.color = "orange";
+const colorchange7 = document.getElementById("caption7");
+colorchange7.addEventListener("click", (event) =>{
+    event.target.style.background = "yellow";
+    event.target.style.color = "black";
 });
 
-const colorchange = document.getElementById("caption");
-colorchange.addEventListener("click", (event) =>{
-    event.target.style.background = "green";
-    event.target.style.color = "orange";
+const colorchange8 = document.getElementById("caption8");
+colorchange8.addEventListener("click", (event) =>{
+    event.target.style.background = "black";
+    event.target.style.color = "yellow";
 });
 
-const colorchange = document.getElementById("caption");
-colorchange.addEventListener("click", (event) =>{
-    event.target.style.background = "green";
-    event.target.style.color = "orange";
+const colorchange9 = document.getElementById("caption9");
+colorchange9.addEventListener("click", (event) =>{
+    event.target.style.background = "orange";
+    event.target.style.color = "green";
 });
 
-const colorchange = document.getElementById("caption");
-colorchange.addEventListener("click", (event) =>{
+const colorchange11 = document.getElementById("caption11");
+colorchange11.addEventListener("click", (event) =>{
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+});
+
+const colorchange10 = document.getElementById("caption10");
+colorchange10.addEventListener("click", (event) =>{
+    event.target.style.background = "yellow";
+    event.target.style.color = "purple";
+});
+
+const colorchange12 = document.getElementById("caption12");
+colorchange12.addEventListener("click", (event) =>{
     event.target.style.background = "green";
     event.target.style.color = "orange";
 });
